4e2d9b2b431309a2a8dbdca45d281dd6e8dfa39f
Francois Robbertze <frobbertze>
2019-01-03 07:04:24 +0200

    addbase database

---
+++ TimeKeeperDB/dbo/Tables/Project.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Project] (
+    [Id]                 INT            IDENTITY (1, 1) NOT NULL,
+    [ProjectName]        VARCHAR (200)  NOT NULL,
+    [ProjectDescription] VARCHAR (8000) NOT NULL,
+    CONSTRAINT [PK_Project] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ TimeKeeperDB/dbo/Tables/TimeLog.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TimeLog] (
+    [Id]        INT      IDENTITY (1, 1) NOT NULL,
+    [CompanyId] INT      NOT NULL,
+    [ProjectId] INT      NOT NULL,
+    [TimeStart] DATETIME NOT NULL,
+    [TimeEnd]   DATETIME NULL,
+    CONSTRAINT [PK_TimeLog] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
